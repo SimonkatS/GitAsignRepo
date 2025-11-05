a84435e4f7fc4ca2e39f01c754ce3ba35e82dd21
SimonkatS <128067555+SimonkatS@users.noreply.github.com>
2025-11-05 18:17:05 +0200

    Testing Branching [testbranch]

--- StudentPerformance_Assignment/StudentPerformance_Assignment.py
+++ StudentPerformance_Assignment/StudentPerformance_Assignment.py
@@ -1,3 +1,10 @@
 import pandas as pd 
 import numpy as np 
 import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import root_mean_squared_error, r2_score
+
+## change here testing the branching
