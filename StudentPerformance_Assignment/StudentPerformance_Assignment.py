71101efa275f83c7d1907cb6a176ceda5ee4532a
SimonkatS <128067555+SimonkatS@users.noreply.github.com>
2025-11-05 19:04:17 +0200

    bullshit

--- StudentPerformance_Assignment/StudentPerformance_Assignment.py
+++ StudentPerformance_Assignment/StudentPerformance_Assignment.py
@@ -8,6 +8,6 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import root_mean_squared_error, r2_score
 
 
-data_path = r"Python\MachineLearningRepo\StudentPerformance_Assignment\student_performance.csv"
+data_path = r"Python/MachineLearningRepo/StudentPerformance_Assignment/student_performance.csv"
 df = pd.read_csv(data_path)
 df.head()
