1b830c293f3be71a14ae4786cbd9185c7909a1db
SimonkatS <128067555+SimonkatS@users.noreply.github.com>
2025-11-05 20:23:57 +0200

    got it working

--- StudentPerformance_Assignment/StudentPerformance_Assignment.py
+++ StudentPerformance_Assignment/StudentPerformance_Assignment.py
@@ -1,12 +1,7 @@
 import pandas as pd 
 import numpy as np 
 import seaborn as sns
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import root_mean_squared_error, r2_score
 
 
-df = pd.read_csv("C:\Users\simon\Desktop\Code File\Python\MachineLearningRepo\StudentPerformance_Assignment\student_performance.csv")
-df.head()
+df = pd.read_csv(r"Python\MachineLearningRepo\StudentPerformance_Assignment\student_performance.csv")
+print(df.describe())
