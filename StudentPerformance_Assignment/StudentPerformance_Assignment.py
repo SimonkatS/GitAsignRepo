8d91c2c8696bf889e12ffbee4aaffeee4a0aa9d8
SimonkatS <128067555+SimonkatS@users.noreply.github.com>
2025-11-06 17:56:06 +0200

    small change, the modified marker is messing with my OCD

--- StudentPerformance_Assignment/StudentPerformance_Assignment.py
+++ StudentPerformance_Assignment/StudentPerformance_Assignment.py
@@ -1,8 +1,15 @@
 import pandas as pd 
 import numpy as np 
 import seaborn as sns
+import sklearn
 
 
 dataset = pd.read_csv(r"Python\MachineLearningRepo\StudentPerformance_Assignment\student_performance.csv")
 print(dataset.describe())
 print(dataset.info())
+
+
+
+
+# Clustering features to work with , dont mind it for now
+X = [weekly_self_study_hours, attendance_percentage, class_participation, total_score]
