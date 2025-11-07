70364f9657d3ccf1ebd421fbdac0e7c12767e85f
SimonkatS <128067555+SimonkatS@users.noreply.github.com>
2025-11-07 13:31:48 +0200

    elbow method, user cluster input, kmeans,stats and graphs

--- StudentPerformance_Assignment/StudentPerformance_Assignment.py
+++ StudentPerformance_Assignment/StudentPerformance_Assignment.py
@@ -1,15 +1,59 @@
 import pandas as pd 
 import numpy as np 
 import seaborn as sns
-import sklearn
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans 
+from sklearn.preprocessing import StandardScaler
+
 
 
 dataset = pd.read_csv(r"Python\MachineLearningRepo\StudentPerformance_Assignment\student_performance.csv")
-print(dataset.describe())
-print(dataset.info())
+# print(dataset.describe())  # helps to see some data from the csv
+features = ["weekly_self_study_hours", "attendance_percentage", "class_participation", "total_score"]
+X = dataset[features]  ## droping 'grade' because its not numerical
+# scaler = StandardScaler()  #scaling so all the features are thought about equally
+# X_scaled = scaler.fit_transform(X)
 
 
+##  ELBOW METHOD TO SEE HOW MANY CLUSTERS ARE OPTIMAL  (PREVIOUS RESULTS SUGGEST K=2 )
+inertias = []
+for k in range(1, 10):
+    km = KMeans(n_clusters=k, random_state=42)
+    km.fit(X)
+    inertias.append(km.inertia_)
+
+plt.plot(range(1, 10), inertias, marker='o')
+plt.xlabel('Number of clusters')
+plt.ylabel('Inertia')
+plt.title('Elbow Method')
+plt.show()
+
+while True:
+    try:
+        k = int(input("Enter the number of clusters (e.g., 2, 3...): "))
+        if k > 0:
+            break
+        else:
+            print("Please enter a positive number.")
+    except ValueError:
+        print("Invalid input. Please enter a number.")
 
 
-# Clustering features to work with , dont mind it for now
-X = [weekly_self_study_hours, attendance_percentage, class_participation, total_score]
+kmean = KMeans(n_clusters=k, random_state=42)  #kmeans is the simplest, works well with the dataset, and we have learned it in class
+kmean.fit(X)
+dataset['Cluster'] = kmean.labels_
+print("Model training complete.")
+
+# Display statistical summary
+print("\n--- Statistical Summary for Each Cluster ---")
+print(dataset.groupby('Cluster')[features].describe().T)
+
+# Display graphical summary (Boxplots)
+print("\nGenerating boxplots for each feature by cluster...")
+for feature in features:
+    plt.figure(figsize=(8, 6))
+    sns.boxplot(data=dataset, x='Cluster', y=feature, palette='Set2')
+    plt.title(f'{feature} Distribution by Cluster')
+    plt.show()
+
+
